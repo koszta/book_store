5447ff82cd3a6faae42fccefe0346aef4c352c26
Péter Pál Koszta <peterpal@fashionunited.com>
2018-03-25 13:22:56 +0200

    Add generated files

---
+++ lib/book_store_web/router.ex
@@ -0,0 +1,11 @@
+defmodule BookStoreWeb.Router do
+  use BookStoreWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", BookStoreWeb do
+    pipe_through :api
+  end
+end
