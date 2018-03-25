b951521e02d1d3ea8882b7fb91d95d1a10a42206
Péter Pál Koszta <peterpal@fashionunited.com>
2018-03-25 20:59:38 +0200

    Serve static data

--- lib/book_store_web/schema.ex
+++ lib/book_store_web/schema.ex
@@ -11,7 +11,33 @@ defmodule BookStoreWeb.Schema do
     field(:isbn, :string)
   end
 
+  @books [
+    %{
+      id: 1,
+      title: "Things that I can not do",
+      author: "Chuck Norris",
+      description:
+        "This book contains so far the greatest collection of things that I can not do.",
+      pages: 0,
+      price: 22.01,
+      isbn: "9786543210984"
+    },
+    %{
+      id: 2,
+      title: "Chest hair as evolutional advantage",
+      author: "David Hasselhoff",
+      description: "A bulletproof chest hair is an essental accessoire of the modern man.",
+      pages: 120,
+      price: 12.99,
+      isbn: "9786543210991"
+    }
+  ]
+
   query do
-    field(:books, list_of(:book))
+    field :books, list_of(:book) do
+      resolve(fn _args, _info ->
+        {:ok, @books}
+      end)
+    end
   end
 end
