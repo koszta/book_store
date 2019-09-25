2e76b5b64d2c0448447669556aceab3254106a3e
Péter Pál Koszta <peterpal@fashionunited.com>
2019-09-25 17:56:41 +0200

    Update schema

--- lib/book_store_web/schema.ex
+++ lib/book_store_web/schema.ex
@@ -2,10 +2,18 @@ defmodule BookStoreWeb.Schema do
   use Absinthe.Schema
   alias BookStoreWeb.BooksResolver
 
+  import_types(Absinthe.Type.Custom)
+
+  object :author do
+    field(:first_name, :string)
+    field(:last_name, :string)
+    field(:date_of_birth, :date)
+  end
+
   object :book do
     field(:id, :id)
     field(:title, :string)
-    field(:author, :string)
+    field(:author, :author)
     field(:description, :string)
     field(:pages, :integer)
     field(:price, :float)
